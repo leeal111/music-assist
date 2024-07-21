f874791defb7110a53e60fe0a5955248c783ffbd
leeal <leeal111@hotmai.com>
2024-07-21 18:58:50 +0800

    fix

--- music.py
+++ music.py
@@ -4,12 +4,13 @@ from cloud import CloudBase
 from musicbase import MusicBase
 from utils import print_info
 from values import CLOUDMUSIC_DATABASE_PATH
+import sys
 
 print("开始执行脚本")
-cloudmusic_path = normpath(r"C:\CloudMusic") # 网易云下载路径
-musicbase_path = normpath(r"D:\音乐") # 音乐库路径
-output_path = normpath(r"C:\Users\fumen\OneDrive\music") # 结果输出路径
-playlist_count = 19 #歌单长度
+cloudmusic_path = normpath(r"C:\CloudMusic")  # 网易云下载路径
+musicbase_path = normpath(r"D:\音乐")  # 音乐库路径
+output_path = normpath(r"C:\Users\fumen\OneDrive\music")  # 结果输出路径
+playlist_count = int(sys.argv[1])  # 歌单长度
 
 # 通过网易云数据库求取歌曲名称列表
 cloudmusic_db = CloudBase(CLOUDMUSIC_DATABASE_PATH)
